9b73389b11b7562f995ed778fef130d0420fcb6a
mariothedog <mariothedogmc@gmail.com>
2019-11-02 19:19:03 +0000

    Placeholder Main Menu

---
+++ Code/Main Menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_Play_Button_pressed():
+	get_tree().change_scene("res://Scenes/Top Down.tscn")
+
+func _on_Play_Button_mouse_entered():
+	$MarginContainer/VBoxContainer/Play.add_color_override("font_color", Color(0.3, 0.3, 0.3))
+	$MarginContainer/VBoxContainer/Play.add_color_override("font_color_shadow", Color(0.8, 0.8, 0.8))
+
+func _on_Play_Button_mouse_exited():
+	$MarginContainer/VBoxContainer/Play.add_color_override("font_color", Color(0, 0, 0))
+	$MarginContainer/VBoxContainer/Play.add_color_override("font_color_shadow", Color(0.61, 0.61, 0.61))
