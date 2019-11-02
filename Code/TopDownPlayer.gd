624fb0e882165e1971a6252ee41abc18208a0834
mariothedog <mariothedogmc@gmail.com>
2019-11-02 12:01:24 +0000

    Basic top down player and area

--- Code/TopDownPlayer.gd
+++ Code/TopDownPlayer.gd
@@ -1,20 +1,15 @@
 extends KinematicBody2D
 
-const GRAVITY = Vector2(0, 400)
-
-export var SPEED = 300
+export var SPEED = 400
 export var ACCEL = 0.1
-export var DEACCEL = 0.9
-export var JUMP_SPEED = 300
+export var DEACCEL = 0.7
 
 var velocity = Vector2()
 var target_vel = Vector2()
 
-var jump = false
-
-func _physics_process(delta):
+func _physics_process(_delta):
 	input()
-	movement(delta)
+	movement()
 
 func input():
 	var input_vel = Vector2()
@@ -23,22 +18,22 @@ func input():
 		input_vel.x += 1
 	if Input.is_action_pressed("move_left"):
 		input_vel.x -= 1
-	
-	jump = false
-	if Input.is_action_just_pressed("jump"):
-		jump = true
+	if Input.is_action_pressed("move_up"):
+		input_vel.y -= 1
+	if Input.is_action_pressed("move_down"):
+		input_vel.y += 1
 	
 	target_vel = input_vel.normalized() * SPEED
 
-func movement(delta):
-	velocity += GRAVITY * delta
-	
+func movement():
 	if abs(target_vel.x) > abs(velocity.x):
 		velocity.x = lerp(velocity.x, target_vel.x, ACCEL)
 	else:
 		velocity.x = lerp(velocity.x, target_vel.x, DEACCEL)
-
-	if jump and is_on_floor():
-		velocity.y = -JUMP_SPEED
 	
-	velocity = move_and_slide(velocity, Vector2.UP)
+	if abs(target_vel.y) > abs(velocity.y):
+		velocity.y = lerp(velocity.y, target_vel.y, ACCEL)
+	else:
+		velocity.y = lerp(velocity.y, target_vel.y, DEACCEL)
+	
+	velocity = move_and_slide(velocity)
