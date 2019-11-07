df5aea4ec964dfdbae0fe729ae69e55d178d33a7
mariothedog <mariothedogmc@gmail.com>
2019-11-07 18:05:58 +0000

    New ground art, jump mechanic WIP

--- Code/Player.gd
+++ Code/Player.gd
@@ -7,39 +7,27 @@ export var JUMP_SPEED = 600
 export var GRAVITY = Vector2(0, 1300)
 
 var velocity = Vector2()
-var target_vel = Vector2()
 
 var jump = false
 
+var jump_dir = position
 var map
 
+func _draw():
+	var pos = get_global_mouse_position()
+	draw_line(Vector2(position.x, position.y + 32), Vector2(pos.x, pos.y+32), Color.red, 2)
+
 func _physics_process(delta):
-	input()
+	update()
 	movement(delta)
 
-func input():
-	var input_vel = Vector2()
-	
-	if Input.is_action_pressed("move_right"):
-		input_vel.x += 1
-	if Input.is_action_pressed("move_left"):
-		input_vel.x -= 1
-	
-	jump = false
-	if Input.is_action_just_pressed("jump"):
-		jump = true
-	
-	target_vel = input_vel.normalized() * SPEED
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.is_pressed() and is_on_floor():
+			jump_dir = get_global_mouse_position()#.normalized() # This is relative to the center unlike event position
+			#velocity = dir * JUMP_SPEED
 
 func movement(delta):
 	velocity += GRAVITY * delta
-	
-	if abs(target_vel.x) > abs(velocity.x):
-		velocity.x = lerp(velocity.x, target_vel.x, ACCEL)
-	else:
-		velocity.x = lerp(velocity.x, target_vel.x, DEACCEL)
-	
-	if jump and is_on_floor():
-		velocity.y = -JUMP_SPEED
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
