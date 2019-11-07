f01f24a869c43be7b49fb201050a3a4c13dd1d4c
mariothedog <mariothedogmc@gmail.com>
2019-11-07 20:00:30 +0000

    Jump Mechanic [No drag yet]

--- Code/Level.gd
+++ Code/Level.gd
@@ -2,3 +2,14 @@ extends Node2D
 
 func _ready():
 	$Player.map = $TileMap
+
+func _draw(): # For debugging
+	var mouse_pos = get_global_mouse_position()
+	draw_line($Player.position, mouse_pos, Color.red, 2)
+
+func _process(_delta):
+	update()
+
+func _on_Player_jump():
+	var jump_dir = get_global_mouse_position().normalized()
+	$Player.velocity = jump_dir * $Player.JUMP_SPEED
--- Code/Player.gd
+++ Code/Player.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
+signal jump
+
 export var SPEED = 400
-export var ACCEL = 0.1
-export var DEACCEL = 0.7
-export var JUMP_SPEED = 600
+export var JUMP_SPEED = 800
 export var GRAVITY = Vector2(0, 1300)
 
 var velocity = Vector2()
@@ -13,19 +13,13 @@ var jump = false
 var jump_dir = position
 var map
 
-func _draw():
-	var pos = get_global_mouse_position()
-	draw_line(Vector2(position.x, position.y + 32), Vector2(pos.x, pos.y+32), Color.red, 2)
-
 func _physics_process(delta):
-	update()
 	movement(delta)
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.is_pressed() and is_on_floor():
-			jump_dir = get_global_mouse_position()#.normalized() # This is relative to the center unlike event position
-			#velocity = dir * JUMP_SPEED
+			emit_signal("jump")
 
 func movement(delta):
 	velocity += GRAVITY * delta
