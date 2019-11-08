33d0aa69a3eb4bf620fc26f4a2d44b51061d034a
mariothedog <mariothedogmc@gmail.com>
2019-11-08 20:00:11 +0000

    Update Pit_Sensor.gd

--- Code/Pit_Sensor.gd
+++ Code/Pit_Sensor.gd
@@ -3,4 +3,5 @@ extends Area2D
 # Pit sensor
 func _on_Pit_sensor_body_entered(body):
 	if body.get_name() == "Player":        # if the body's name is player, the scene will reload
-		get_tree().reload_current_scene()
+		if get_tree().reload_current_scene() != OK:
+			print_debug("An error occured when trying to reload the current scene at Pit_Sensor.gd.")
