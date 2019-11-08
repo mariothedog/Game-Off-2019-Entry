7f3e3490fe013bcb87b8fc835f1411d12d5dcc34
aZaamBie <azaamholdman@Gmail.com>
2019-11-08 20:03:02 +0200

    Pit sensor
    I've made a pit sensor scene and made the script. I've also added it to level 1

---
+++ Code/Pit_Sensor.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+# Pit sensor
+func _on_Pit_sensor_body_entered(body):
+	if body.get_name() == "Player":        # if the body's name is player, the scene will reload
+		get_tree().reload_current_scene()
