aa430d00797e761cc12b26ea0c8e9dcd2ce525e6
mariothedog <mariothedogmc@gmail.com>
2019-11-27 20:02:55 +0000

    Update SpikeTrap.gd

--- SpikeTrap.gd
+++ SpikeTrap.gd
@@ -6,15 +6,16 @@ export var KNOCKBACK = Vector2(400, 0)
 var can_attack = true
 
 func _on_SpikeTrap_body_entered(body):
-	if body.name == "Player" and can_attack and not body.dead:
+	if body.name == "Player" and can_attack:
 		damage()
 
 func damage():
-	global.player.take_damage(DAMAGE)
-	var dir = (global.player.position - position).normalized()
-	var kb = KNOCKBACK.rotated(dir.angle())
-	global.player.knockback(kb)
-	$"Attack Again".start()
+	if not global.player.dead:
+		global.player.take_damage(DAMAGE)
+		var dir = (global.player.position - position).normalized()
+		var kb = KNOCKBACK.rotated(dir.angle())
+		global.player.knockback(kb)
+		$"Attack Again".start()
 
 func _on_SpikeTrap_body_exited(body):
 	$"Attack Again".stop()
