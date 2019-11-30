e36a4cafdd984231d2bd83ca73586914ac353add
mariothedog <mariothedogmc@gmail.com>
2019-11-30 18:20:53 +0000

    Falling into the void no longer gives you 3 extra lives + enemy added to level 2

--- Code/Pit_Sensor.gd
+++ Code/Pit_Sensor.gd
@@ -2,5 +2,5 @@ extends Area2D
 
 # Pit sensor
 func _on_Pit_sensor_body_entered(body):
-	if body.get_name() == "Player":        # if the body's name is player, the scene will reload
-		body.die()
+	if body.get_name() == "Player": # if the body's name is player, the scene will reload
+		body.take_damage(100)
