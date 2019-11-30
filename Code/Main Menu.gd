4da31948f93a45d7537dcf2cf75f982e3319c205
aZaamBie <azaamholdman@Gmail.com>
2019-11-30 17:57:58 +0200

    Play Button and Credits button added
    I've re-made the play and credits button.
    I've added them into the Main menu.

--- Code/Main Menu.gd
+++ Code/Main Menu.gd
@@ -5,16 +5,31 @@ func _on_Play_Button_pressed():
 	Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	$Transition.start()
 
+func _on_Credits_Button_pressed():
+	pass # Replace with function body.
+
 func _process(_delta):
 	if modulate.a <= 0:
 		if get_tree().change_scene("res://Scenes/Level 1.tscn") != OK:
 			print_debug("An error occured when trying to switch from the Main Menu scene to the level scene.")
 
+# PLAY BUTTON
 func _on_Play_Button_mouse_entered():
 	$MarginContainer/VBoxContainer/Play.add_color_override("font_color", Color(0.3, 0.3, 0.3))
 	$MarginContainer/VBoxContainer/Play.add_color_override("font_color_shadow", Color(0.8, 0.8, 0.8))
 	$"Mouse Button Overlap SFX".play()
 
 func _on_Play_Button_mouse_exited():
+	$MarginContainer/VBoxContainer/Play.add_color_override("font_color", Color(0, 0, 0))
+	$MarginContainer/VBoxContainer/Play.add_color_override("font_color_shadow", Color(0.61, 0.61, 0.61))
+
+# CREDITS BUTTON
+
+func _on_Credits_Button_mouse_entered():
+	$MarginContainer/VBoxContainer/Play.add_color_override("font_color", Color(0.3, 0.3, 0.3))
+	$MarginContainer/VBoxContainer/Play.add_color_override("font_color_shadow", Color(0.8, 0.8, 0.8))
+	$"Mouse Button Overlap SFX".play()
+
+func _on_Credits_Button_mouse_exited():
 	$MarginContainer/VBoxContainer/Play.add_color_override("font_color", Color(0, 0, 0))
 	$MarginContainer/VBoxContainer/Play.add_color_override("font_color_shadow", Color(0.61, 0.61, 0.61))
